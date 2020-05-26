036cd916198e6d2636b63648e233d3da56186d17
Tak-John Cheung <tak_john@hotmail.com>
2020-05-26 23:20:39 +0100

    updated pattern and screenshot

--- src/game.ts
+++ src/game.ts
@@ -12,7 +12,7 @@ const scene = new Entity()
 scene.addComponent(
   new Transform({
     position: new Vector3(8, 0, 8),
-    rotation: Quaternion.Euler(0, 0, 0),
+    rotation: Quaternion.Euler(0, 90, 0),
   })
 )
 engine.addEntity(scene)
@@ -22,26 +22,34 @@ const boxShape = new BoxShape()
 boxShape.withCollisions = false
 
 // Parameters for the mural
-const MURAL_WIDTH = 32
+const MURAL_WIDTH = 33
 const MURAL_HEIGHT = 16
-const START_POS_X = -3.69
+const START_POS_X = -4.445
 const START_POS_Y = 4.5
 const TILE_SIZE = 0.25
 
 let xPosition = START_POS_X
 let yPosition = START_POS_Y
 let tileIndex = 0
+let isRed = true
 
 // Build mural
 for (let i = 0; i < MURAL_HEIGHT; i++) {
   for (let j = 0; j < MURAL_WIDTH; j++) {
+    // Default color pattern (brick)
+    let colorIndex: number
+    if (i % 2 != 0) isRed = !isRed
+    if (j % 2 != 0) isRed = !isRed
+    isRed ? (colorIndex = 1) : (colorIndex = 2)
+
     const tile = new Tile(
       boxShape,
       new Transform({
         position: new Vector3(xPosition, yPosition, 0),
         scale: new Vector3(TILE_SIZE, TILE_SIZE, 0.125),
       }),
-      tileIndex
+      tileIndex,
+      colorIndex
     )
     tile.setParent(scene)
     tileIndex = tiles.push(tile)
