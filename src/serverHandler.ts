f8902c6be62bd4873eb57aad91a3fea5d995ade4
nearnshaw <nearnshaw@decentraland.org>
2020-06-08 16:17:45 -0300

    clean up server

--- src/serverHandler.ts
+++ src/serverHandler.ts
@@ -1,5 +1,6 @@
 import { tileNumbers } from './tile'
 import { setRealm, playerRealm } from './realmData'
+import utils from '../node_modules/decentraland-ecs-utils/index'
 
 export let awsServer = 'https://genesis-plaza.s3.us-east-2.amazonaws.com/'
 export let fireBaseServer =
@@ -22,20 +23,29 @@ export async function getMural(): Promise<number[]> {
 
 // update mural
 export async function changeMural() {
-  try {
-    // if (!playerRealm) {
-    //   await setRealm()
-    // }
-    let url = fireBaseServer + 'update-mural?realm=' + playerRealm
-    let body = JSON.stringify({ tiles: tileNumbers })
-    let headers = {}
-    let response = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: body,
+  // if (!playerRealm) {
+  //   await setRealm()
+  // }
+  muralChanger.addComponentOrReplace(
+    // Only send request if no more changes come over the next second
+    new utils.Delay(1000, async function () {
+      try {
+        let url = fireBaseServer + 'update-mural?realm=' + playerRealm
+        let body = JSON.stringify({ tiles: tileNumbers })
+        let headers = {}
+        let response = await fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: body,
+        })
+        return response.json()
+      } catch {
+        log('error fetching from AWS server')
+      }
     })
-    return response.json()
-  } catch {
-    log('error fetching from AWS server')
-  }
+  )
 }
+
+// dummy entity to throttle the sending of change requests
+let muralChanger = new Entity()
+engine.addEntity(muralChanger)
